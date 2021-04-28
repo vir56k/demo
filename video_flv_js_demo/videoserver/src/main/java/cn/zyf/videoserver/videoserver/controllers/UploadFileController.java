34c5b76333fbbb6676e6692867491f4e05c03f5d
zhangyunfei <zhangyunfei@aerozhonghuan.com>
2021-04-28 18:08:47 +0800

    编写一个 直接使用flv.js 的 DEMO

---
+++ video_flv_js_demo/videoserver/src/main/java/cn/zyf/videoserver/videoserver/controllers/UploadFileController.java
@@ -0,0 +1,16 @@
+package cn.zyf.videoserver.videoserver.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: zhangyunfei
+ * @date: 2021/4/25 19:22
+ */
+@RestController
+@RequestMapping()
+public class UploadFileController {
+    private static final String TAG = UploadFileController.class.getSimpleName();
+
+}
---
+++ video_flv_js_demo/videoserver/src/test/java/cn/zyf/videoserver/videoserver/VideoserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zyf.videoserver.videoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
